33eb96f3077d549c1609f810a4a7a9bfa85f6332
tsopsericado <ricardotsopse@gmail.com>
2023-07-30 04:03:42 +0100

    all the necessary components needed created and styled

---
+++ src/components/Transaction.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Transaction(transaction) {
+ const sign = transaction.amount < 0 ? '-' : '+';
+
+  return (
+    <li className="minus">
+      {transaction.text}
+      <span>{sign}${Math.abs(transaction.amount)}</span>
+      <button className="delete-btn">x</button>
+    </li>
+  );
+}
